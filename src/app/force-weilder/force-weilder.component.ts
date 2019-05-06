0d804992918a897f8242d6431f2173e48f877625
petra <petra.strucic@gmail.com>
2019-05-06 02:09:34 +0200

    *ngIf, *ngFor directives, event binding

--- src/app/force-weilder/force-weilder.component.ts
+++ src/app/force-weilder/force-weilder.component.ts
@@ -1,22 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { ForceWeilder } from '../force-weilder';
+import { WEILDERS } from '../mock-force-weilder';
 
 @Component({
   selector: 'app-force-weilder',
   templateUrl: './force-weilder.component.html',
   styleUrls: ['./force-weilder.component.css']
 })
+
 export class ForceWeilderComponent implements OnInit {
 
-  forceWeilder: ForceWeilder = {
-    id : 1,
-    name : "Mace Windu",
-    force : 42
-  }
+  // forceWeilder: ForceWeilder = {
+  //   id : 1,
+  //   name : "Mace Windu",
+  //   force : 42
+  // }
+
+  weilders = WEILDERS;
+  selectedForceWeilder: ForceWeilder;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSelect(forceWeilder: ForceWeilder): void {
+    this.selectedForceWeilder = forceWeilder;
+  }
   // forceWeilder = "Mace Windu";
 }
